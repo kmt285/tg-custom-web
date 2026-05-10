9926670018739c83e227ad65cd5443fb887ecc14
kmt285 <248855908+kmt285@users.noreply.github.com>
2026-05-10 13:51:11 +0630

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.express.Router();
+const router = express.Router();
 const { sendCode, verifyCode } = require('../controllers/authController');
 
 router.post('/send-code', sendCode);
