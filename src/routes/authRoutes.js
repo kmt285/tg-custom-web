ad7c75e7053899e9d3796230e14ff03453d4332f
kmt285 <248855908+kmt285@users.noreply.github.com>
2026-05-11 11:41:57 +0630

    Delete src directory

--- src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendCode, verifyCode } = require('../controllers/authController');
-
-router.post('/send-code', sendCode);
-router.post('/verify-code', verifyCode);
-
-module.exports = router;
